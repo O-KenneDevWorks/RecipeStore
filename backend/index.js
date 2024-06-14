d3c66d3699c5be703f5624477dde64f83d6fa0db
LuciferGreyInc <79246841+LuciferGreyInc@users.noreply.github.com>
2024-06-14 16:02:00 -0400

    Removed Deprecated functions

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,7 @@ app.use(cors());
 app.use(bodyParser.json());
  
 mongoose.connect('mongodb://10.0.0.85:27017/recipeStore', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Failed to connect to MongoDB', err));
