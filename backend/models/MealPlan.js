ac588e4399f86b2a8e5e843cba35fd79cba7b0f6
LuciferGreyInc <79246841+LuciferGreyInc@users.noreply.github.com>
2024-06-24 19:28:17 -0400

    Update MealPlan.js

--- backend/models/MealPlan.js
+++ backend/models/MealPlan.js
@@ -20,3 +20,27 @@ const MealPlanSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('MealPlan', MealPlanSchema);
+
+
+// const mongoose = require('mongoose');
+
+// const mealSchema = new mongoose.Schema({
+//     dayOfWeek: {
+//         type: String,
+//         enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+//         required: true
+//     },
+//     mainCourse: { type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' },
+//     sides: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }]
+// });
+
+// const weekMealPlanSchema = new mongoose.Schema({
+//     weekOfYear: Number,
+//     year: Number,
+//     meals: [mealSchema],
+//     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+// });
+
+// const WeekMealPlan = mongoose.model('WeekMealPlan', weekMealPlanSchema);
+
+// module.exports = WeekMealPlan;
