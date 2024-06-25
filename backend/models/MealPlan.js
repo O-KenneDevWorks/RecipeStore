bc31efffd99a9023b98e731e8f11e19400482b97
LuciferGreyInc <79246841+LuciferGreyInc@users.noreply.github.com>
2024-06-24 20:07:46 -0400

    Update MealPlan.js

--- backend/models/MealPlan.js
+++ backend/models/MealPlan.js
@@ -1,41 +1,41 @@
-// const mongoose = require('mongoose');
-
-// const MealPlanSchema = new mongoose.Schema({
-//     week: Number,
-//     year: Number,
-//     days: [{
-//         mainCourse: {
-//             type: mongoose.Schema.Types.ObjectId,
-//             ref: 'Recipe'
-//         },
-//         sides: [{
-//             type: mongoose.Schema.Types.ObjectId,
-//             ref: 'Recipe'
-//         }]
-//     }]
-//     // createdBy: {
-//     //     type: mongoose.Schema.Types.ObjectId,
-//     //     ref: 'User' // Assuming you have a user model for user management
-//     // }
-// });
-
-// module.exports = mongoose.model('MealPlan', MealPlanSchema);
-
-
 const mongoose = require('mongoose');
 
-const mealSchema = new mongoose.Schema({
-    dayOfWeek: String,
-    mainCourse: { type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' },
-    sides: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }]
-});
-
-const weekMealPlanSchema = new mongoose.Schema({
+const MealPlanSchema = new mongoose.Schema({
+    week: Number,
     year: Number,
-    weekOfYear: Number,
-    meals: [mealSchema],
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }  // Assuming user authentication
+    days: [{
+        mainCourse: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Recipe'
+        },
+        sides: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Recipe'
+        }]
+    }]
+    // createdBy: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'User' // Assuming you have a user model for user management
+    // }
 });
 
-const WeekMealPlan = mongoose.model('WeekMealPlan', weekMealPlanSchema);
-module.exports = WeekMealPlan;
+module.exports = mongoose.model('MealPlan', MealPlanSchema);
+
+
+// const mongoose = require('mongoose');
+
+// const mealSchema = new mongoose.Schema({
+//     dayOfWeek: String,
+//     mainCourse: { type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' },
+//     sides: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }]
+// });
+
+// const weekMealPlanSchema = new mongoose.Schema({
+//     year: Number,
+//     weekOfYear: Number,
+//     meals: [mealSchema],
+//     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }  // Assuming user authentication
+// });
+
+// const WeekMealPlan = mongoose.model('WeekMealPlan', weekMealPlanSchema);
+// module.exports = WeekMealPlan;
