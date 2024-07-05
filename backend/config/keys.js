2389ed09e9945f2363d69420e802f2e3eab219e1
LuciferGreyInc <79246841+LuciferGreyInc@users.noreply.github.com>
2024-07-05 08:31:22 -0400

    updating login info

---
+++ backend/config/keys.js
@@ -0,0 +1,8 @@
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+
+module.exports = {
+    mongoURI: `mongodb+srv://${dbUser}:${dbPassword}@your-cluster-url/${dbName}?retryWrites=true&w=majority`,
+    secretOrKey: process.env.SECRET_OR_KEY // Secret key for JWT
+};
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+require('dotenv').config();
 
 const users = require('./routes/users');
 
