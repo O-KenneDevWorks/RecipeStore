9bf4fa109c0b1d5c54a725f4179b07be8dbfda13
LuciferGreyInc <79246841+LuciferGreyInc@users.noreply.github.com>
2024-07-10 17:50:08 -0400

    Update App.js

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -19,7 +19,7 @@ const Home = () => {
     useEffect(() => {
         const fetchRecipes = async () => {
             try {
-                const response = await axios.get('/recipes/previews');  // Updated endpoint
+                const response = await axios.get('http://10.0.0.85:3000/recipes/previews');  // Updated endpoint
                 setRecipes(response.data);  // Set the fetched recipes to state
             } catch (error) {
                 console.error('Failed to fetch recipe previews:', error);
