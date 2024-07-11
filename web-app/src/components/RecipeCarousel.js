324e6c8515c662b4b6c2444b8dd7b2d84fb1aeec
LuciferGreyInc <79246841+LuciferGreyInc@users.noreply.github.com>
2024-07-11 19:36:51 -0400

    Did things

--- web-app/src/components/RecipeCarousel.js
+++ web-app/src/components/RecipeCarousel.js
@@ -19,12 +19,22 @@ const RecipeCarousel = () => {
         fetchRecipes();
     }, []);
 
+    // URL to a placeholder image
+    const placeholderImg = '../No_Photo.jpg'; // Replace with your actual placeholder image URL
+
     return (
         <div className="carousel-container">
             <div className="carousel-track">
                 {recipes.map((recipe) => (
                     <Link key={recipe._id} to={`http://10.0.0.85:3000/recipe/${recipe._id}`} className="carousel-item">
-                        <img src={recipe.image} alt={recipe.name} />
+                        <img
+                            src={recipe.image}
+                            alt={recipe.name}
+                            onError={(e) => {
+                                e.target.onerror = null; // Prevents recursion if the placeholder image also fails
+                                e.target.src = placeholderImg; // Fallback to placeholder image
+                            }}
+                        />
                         <h3>{recipe.name}</h3>
                     </Link>
                 ))}
