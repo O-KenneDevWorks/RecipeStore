80643e2ea3b853a62a737f7af73271186382d2cf
OKenne <175995429+O-KenneDevWorks@users.noreply.github.com>
2024-12-17 22:34:17 -0500

    Abstracted UnitOptions

--- web-app/src/constants/options.ts
+++ web-app/src/constants/options.ts
@@ -21,3 +21,47 @@ export const CUISINE_OPTIONS = [
     "Mexican",
     "Thai",
 ];
+
+export const SUGGESTED_TAGS = [
+    "Untried",
+    "Liked",
+    "Favourite",
+    "Spring",
+    "Summer",
+    "Autumn",
+    "Winter",
+]
+
+interface UnitOption {
+    value: string;
+    label: string;
+}
+
+export const UnitOptions: UnitOption[] = [
+    { value: 'Teaspoon', label: 'Teaspoon (tsp)' },
+    { value: 'Tablespoon', label: 'Tablespoon (tbsp)' },
+    { value: 'Fluid Ounce', label: 'Fluid Ounce (fl oz)' },
+    { value: 'Cup', label: 'Cup' },
+    { value: 'Pint', label: 'Pint (pt)' },
+    { value: 'Quart', label: 'Quart (qt)' },
+    { value: 'Gallon', label: 'Gallon (gal)' },
+    { value: 'Milliliter', label: 'Milliliter (ml)' },
+    { value: 'Liter', label: 'Liter (l)' },
+    { value: 'Deciliter', label: 'Deciliter (dl)' },
+    { value: 'Ounce', label: 'Ounce (oz)' },
+    { value: 'Pound', label: 'Pound (lb)' },
+    { value: 'Gram', label: 'Gram (g)' },
+    { value: 'Kilogram', label: 'Kilogram (kg)' },
+    { value: 'Inch', label: 'Inch (in)' },
+    { value: 'Centimeter', label: 'Centimeter (cm)' },
+    { value: 'Millimeter', label: 'Millimeter (mm)' },
+    { value: 'Each', label: 'Each' },
+    { value: 'Dozen', label: 'Dozen' },
+    { value: 'Pinch', label: 'Pinch' },
+    { value: 'Dash', label: 'Dash' },
+    { value: 'Smidgen', label: 'Smidgen' },
+    { value: 'Handful', label: 'Handful' },
+    { value: 'Bunch', label: 'Bunch' },
+    { value: 'Degrees Fahrenheit', label: 'Degrees Fahrenheit (°F)' },
+    { value: 'Degrees Celsius', label: 'Degrees Celsius (°C)' }
+];
